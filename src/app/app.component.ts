92746ca6285d08f170d23753e4bc975ec1e9b876
Tim Barani <timb63701@gmail.com>
2023-04-01 16:41:14 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import '@vaadin/grid/vaadin-grid.js';
+import '@vaadin/grid/vaadin-grid-selection-column.js';
+import '@vaadin/grid/vaadin-grid-sort-column.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'vaadin-app';
+  data: any[] = [];
+
+  constructor(){
+
+  }
+
+  ngAfterViewInit(): void {
+    fetch('https://demo.vaadin.com/demo-data/1.0/people?count=200')
+      .then((res) => res.json())
+      .then((json) => (this.data = json.result));
+  }
 }
